d1aa6ff966644f13f1e2ca5302a4f0ba21aae1ab
nirglus <nirgluskin@gmail.com>
2023-11-11 04:06:40 +0200

    add calcTotal

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function calcTip(bill, tip, people){
     let tipEach = bill * (tip / 100);
     return tipEach / people;
 }
+function calcTotal(bill, tip, people){
+    let totalSum = bill * (tip / 100) + bill;
+    let totalEach = totalSum / people;
+    return totalEach;
+}
