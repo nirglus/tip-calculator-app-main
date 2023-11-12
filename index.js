155c80b9a242d1a3e1a82c06cf4e74c588c015d3
nirglus <nirgluskin@gmail.com>
2023-11-12 10:09:07 +0200

    add DOMContentLoaded

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-const billInput = document.getElementById("billInput");
-const tipBtns = document.querySelectorAll(".tip-btns button");
-const customTipInput = document.getElementById("tip-input");
-const pplNumInput = document.getElementById("pplNum");
-const displayTip = document.getElementById("tipDisp");
-const displayTotal = document.getElementById("totalDisp");
-let lastChangedInput;
-
-function calcTip(bill, tip, people){
-    let tipEach = bill * (tip / 100);
-    return tipEach / people;
-}
-function calcTotal(bill, tip, people){
-    let totalSum = bill * (tip / 100) + bill;
-    let totalEach = totalSum / people;
-    return totalEach;
-}
-
-function calculate(){
-    const billValue = parseFloat(billInput.value) || 0;
-    const tipPercentage = lastChangedInput.id === "tip-input" ? parseFloat(customTipInput.value) || 0 : parseFloat(lastChangedInput.innerHTML);
+document.addEventListener("DOMContentLoaded", () =>{
+    const billInput = document.getElementById("billInput");
+    const tipBtns = document.querySelectorAll(".tip-btns button");
+    const customTipInput = document.getElementById("tip-input");
+    const pplNumInput = document.getElementById("pplNum");
+    const displayTip = document.getElementById("tipDisp");
+    const displayTotal = document.getElementById("totalDisp");
+    let lastChangedInput;
     
-    const pplNumValue = parseFloat(pplNumInput.value) || 1;
-    const tipAmountPerPerson = calcTip(billValue, tipPercentage, pplNumValue);
-    const totalAmountPerPerson = calcTotal(billValue, tipPercentage, pplNumValue);
+    function calcTip(bill, tip, people){
+        let tipEach = bill * (tip / 100);
+        return tipEach / people;
+    }
+    function calcTotal(bill, tip, people){
+        let totalSum = bill * (tip / 100) + bill;
+        let totalEach = totalSum / people;
+        return totalEach;
+    }
     
-    displayTip.innerHTML = `${tipAmountPerPerson.toFixed(2)}`;
-    displayTotal.innerHTML = `${totalAmountPerPerson.toFixed(2)}`;
-}
-
-function handleInputChange(event){
-    lastChangedInput = event.target;
-    calculate();
-}
-
-billInput.addEventListener("input", handleInputChange);
-tipBtns.forEach(btn => btn.addEventListener("click", handleInputChange));
-customTipInput.addEventListener("input", handleInputChange);
-pplNumInput.addEventListener("input", handleInputChange);
+    function calculate(){
+        const billValue = parseFloat(billInput.value) || 0;
+        const tipPercentage = lastChangedInput.id === "tip-input" ? parseFloat(customTipInput.value) || 0 : parseFloat(lastChangedInput.innerHTML);
+        
+        const pplNumValue = parseFloat(pplNumInput.value) || 1;
+        const tipAmountPerPerson = calcTip(billValue, tipPercentage, pplNumValue);
+        const totalAmountPerPerson = calcTotal(billValue, tipPercentage, pplNumValue);
+        
+        displayTip.innerHTML = `${tipAmountPerPerson.toFixed(2)}`;
+        displayTotal.innerHTML = `${totalAmountPerPerson.toFixed(2)}`;
+    }
+    
+    function handleInputChange(event){
+        lastChangedInput = event.target;
+        calculate();
+    }
+    
+    billInput.addEventListener("input", handleInputChange);
+    tipBtns.forEach(btn => btn.addEventListener("click", handleInputChange));
+    customTipInput.addEventListener("input", handleInputChange);
+    pplNumInput.addEventListener("input", handleInputChange);
+});
 
 
 
