bc8545be655441520d2d52382824a89f636e5cc0
nirglus <nirgluskin@gmail.com>
2023-11-12 09:54:25 +0200

    declare dom elements

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const billInput = document.getElementById("billInput");
+const tipBtns = document.querySelectorAll(".tip-btns button");
+const customTipInput = document.getElementById("tip-input");
+const pplNumInput = document.getElementById("pplNum");
+
 function calcTip(bill, tip, people){
     let tipEach = bill * (tip / 100);
     return tipEach / people;
