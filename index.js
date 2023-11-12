6c8d75a1d29dde579e6347fdbc2d683b96103b9e
nirglus <nirgluskin@gmail.com>
2023-11-12 10:04:05 +0200

    add calc func

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const billInput = document.getElementById("billInput");
 const tipBtns = document.querySelectorAll(".tip-btns button");
 const customTipInput = document.getElementById("tip-input");
 const pplNumInput = document.getElementById("pplNum");
+const displayTip = document.getElementById("tipDisp");
+const displayTotal = document.getElementById("totalDisp");
+let lastChangedInput;
 
 function calcTip(bill, tip, people){
     let tipEach = bill * (tip / 100);
@@ -12,3 +15,23 @@ function calcTotal(bill, tip, people){
     let totalEach = totalSum / people;
     return totalEach;
 }
+
+function calculate(){
+    const billValue = parseFloat(billInput.value) || 0;
+    const tipPercentage = lastChangedInput.id === 'tip-input' ? parseFloat(customTipInput.value) || 0 : parseFloat(lastChangedInput.innerHTML);
+    
+    const pplNumValue = parseFloat(pplNumInput.value) || 1;
+    const tipAmountPerPerson = calcTip(billValue, tipPercentage, pplNumValue);
+    const totalAmountPerPerson = calcTotal(billValue, tipPercentage, pplNumValue);
+    
+    displayTip.innerHTML = `${tipAmountPerPerson.toFixed(2)}`;
+    displayTotal.innerHTML = `${totalAmountPerPerson.toFixed(2)}`;
+}
+
+function handleInputChange(event){
+    lastChangedInput = event.target;
+    calculate();
+}
+
+
+
